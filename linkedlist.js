42bea45f95f46ac40bd288bfe240f81627c04204
MarcosGTS <marcosgtsodre@gmail.com>
2023-08-27 17:19:43 -0300

    Add basic structure

---
+++ linkedlist.js
@@ -0,0 +1,50 @@
+function Node(value = null, next = null) {
+    let data = value;
+    let nextNode = next;
+
+    function getValue() {}
+    function setValue(value) {}
+    function getNext() {}
+    function setNext(next) {}
+
+    return {
+        getValue,
+        setValue,
+        getNext,
+        setNext,
+    }
+}
+
+function LinkedList() {
+    let head = null;
+    let size = 0;
+    
+    function append(value) {}
+    function prepend(value {}
+    function getSize() {}
+    function getHead() {}
+    function getTail() {}
+    function at(index) {}
+    function pop() {}
+    function contains(value) {}
+    function find(value) {}
+    function toString() {}
+    function insertAt(value, index) {}
+    function removeAt(index) {}
+
+    return {
+        append,
+        prepend,
+        size,
+        head,
+        tail,
+        at,
+        pop,
+        contains,
+        find,
+        toString,
+        insertAt,
+        removeAt,
+    }
+}
+
